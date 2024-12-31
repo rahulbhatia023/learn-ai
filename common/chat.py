32cda0666f8f8c9099f729aab2ba13fb831dbdd4
Rahul Bhatia <rahul.bhatia023@gmail.com>
2024-12-31 14:40:32 -0500

    Added financial analyst agent

--- common/chat.py
+++ common/chat.py
@@ -1,3 +1,5 @@
+import time
+
 import streamlit as st
 
 
@@ -5,8 +7,17 @@ def add_chat_message(agent_name: str, role: str, content: str):
     st.session_state.page_messages[agent_name].append(
         {"role": role, "content": content}
     )
+
+    def stream_data():
+        for word in content.split(" "):
+            yield word + " "
+            time.sleep(0.04)
+
     with st.chat_message(role):
-        st.write(content)
+        if role == "ai":
+            st.write_stream(stream_data)
+        else:
+            st.write(content)
 
 
 def display_message(agent_name: str, v):
